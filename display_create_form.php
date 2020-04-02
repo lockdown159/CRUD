12a97019af8ee48886118e7569de9b9c147d4cf7
Ricky <43097884+lockdown159@users.noreply.github.com>
2020-04-02 15:41:06 -0400

    Delete display_create_form.php

--- display_create_form.php
+++
@@ -1,5 +0,0 @@
-<h1>Create/add new message</h1>
-<form method='post' action='insert_record.php'>
-    message: <input name='msg' type='text' ><br />
-    <input type="submit" value="Submit">
-</form>
