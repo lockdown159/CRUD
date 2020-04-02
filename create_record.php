76a5eb3c400150a9eb359c9b10fe25cd29d8d9bc
Ricky <43097884+lockdown159@users.noreply.github.com>
2020-04-02 15:40:57 -0400

    Delete create_record.php

--- create_record.php
+++
@@ -1,8 +0,0 @@
-<form methof ='post' action='insert_record.php'>
-
-	First Name: <input name = 'fname' type='text' /><br>
-	Last Name: <input name = 'lname' type='text' /><br>
-	Phone Num: <input name = 'phone' type='text' /><br>
-
-    <button type='input()'> Submit</button>
-</form>
